76ce1e273f9f76623c9a67ad84c76ab56813a3cb
rajksaha <raj.gmk@gmail.com>
2016-03-12 03:05:03 +0800

    Updated code from doctor visit

---
+++ javascript/controllers/oldPrescription/oldPrescription.js
@@ -0,0 +1,303 @@
+app.controller('OldPrescriptionController', function($scope, $http, $modal, $rootScope, limitToFilter, $location) {
+	
+	$scope.patientData = {};
+	$scope.oldAppoinmentList =[];
+	$scope.appoinmentData ={};
+	$scope.patientStateList = [];
+	
+	$scope.history1 = "MH";
+	$scope.history2 = "OBS";
+	
+	$scope.bringPatientInfo = function(){
+		
+		var dataString = "query=0";
+
+        $http({
+            method: 'POST',
+            url: "phpServices/prescription/prescriptionHelperService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.patientData = result;
+        	
+        	$scope.bringPatientOldPrescription($scope.patientData.patientID)
+        });
+	};
+	
+    
+	$scope.bringPatientOldPrescription = function (patientID){
+    	
+		var dataString = "query=0" + '&patientID=' + patientID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/oldPrescription/oldPrescription.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.oldAppoinmentList = result;
+        	$scope.numberOfPrescription = $scope.oldAppoinmentList.length;
+        });
+    };
+    
+    $scope.prescribedDrugList = [];
+	
+	$scope.bringPresCribedDrugs = function (appointmentID){
+		
+		var dataString = "query=0" + '&appointmentID=' + appointmentID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.prescribedDrugList = result;
+        });
+	};
+	
+	$scope.prescribedInvData = [];
+	
+	$scope.bringPrescribedInv = function (appointmentID){
+		
+		$scope.invAdderData = {};
+		
+		var dataString = "query=1" + '&appointmentID=' + appointmentID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.prescribedInvData = result;
+        });
+	};
+	
+	$scope.prescribedAdviceData = [];
+	
+	$scope.bringPrescribedAdvice = function(appointmentID){
+		
+		var dataString = "query=2" + '&appointmentID=' + appointmentID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.prescribedAdviceData = result;
+        });
+		
+	};
+	
+	$scope.prescribedVitalData = [];
+	
+	$scope.bringPrescribedVital = function(appointmentID){
+		
+		var dataString = "query=3" + '&appointmentID=' + appointmentID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.prescribedVitalData = result;
+        });
+		
+	};
+	
+	$scope.prescribedComplainData = [];
+	
+	$scope.bringPrescribedComplain = function(appointmentID){
+		
+		var dataString = "query=4" + '&appointmentID=' + appointmentID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.prescribedComplainData = result;
+        });
+		
+	};
+	
+	$scope.prescribedMHData = [];
+	
+	$scope.bringPrescribedMH = function(appointmentID, patientID){
+		
+		var dataString = "query=5" + '&typeCode=MH' + '&appointmentID=' + appointmentID + '&patientID=' + patientID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.prescribedMHData = result;
+        });
+		
+	};
+	
+	$scope.prescribedOBSData = [];
+	
+	$scope.bringPrescribedOBS = function(appointmentID, patientID){
+		
+		var dataString = "query=5" + '&typeCode=OBS' + '&appointmentID=' + appointmentID + '&patientID=' + patientID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.prescribedOBSData = result;
+        });
+		
+	};
+	
+	$scope.prescribedDiagnosisData = [];
+	
+	$scope.bringPrescribedDiagnosis = function(appointmentID){
+		
+		var dataString = "query=6" + '&appointmentID=' + appointmentID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.prescribedDiagnosisData = result;
+        });
+		
+	};
+	
+	$scope.diagnosisData = {};
+	
+	$scope.bringPresCribedDiagnosis = function (appointmentID){
+		
+		var dataString = "query=6" + '&appointmentID=' + appointmentID;
+
+        $http({
+            method: 'POST',
+            url: "phpServices/commonServices/prescriptionDetailService.php",
+            data: dataString,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (result) {
+        	$scope.diagnosisData = result;
+        });
+	};
+	
+	$scope.historyList = [];
+	    
+	    $scope.bringPrescribedHistory = function(appointmentID, patientID){
+	    	$scope.historyList = [];
+	    	angular.forEach($scope.menuDataList, function(value, key) {
+	    		if(value.inPrescription == 2){
+	    			var dataString = "query=5" + '&typeCode='+ value.defaultName  + '&appointmentID=' + appointmentID + '&patientID=' + patientID;
+	
+	    	        $http({
+	    	            method: 'POST',
+	    	            url: "phpServices/commonServices/prescriptionDetailService.php",
+	    	            data: dataString,
+	    	            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+	    	        }).success(function (result) {
+	    	        	if(result){
+	    	        		var historyData = {};
+	        	        	historyData.headerName = value.menuHeader;
+	        	        	historyData.prescribedHistoryList = result;
+	        	        	$scope.historyList.push(historyData);
+	    	        	}
+	    	        });
+	    		}
+	    	});
+	    	
+	    };
+	    
+	    $scope.bringMenu = function(){
+			
+			var dataString = "query=1";
+
+	        $http({
+	            method: 'POST',
+	            url: "phpServices/prescription/prescriptionHelperService.php",
+	            data: dataString,
+	            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+	        }).success(function (result) {
+	        	$scope.menuDataList = result;
+	        });
+			
+		};
+    
+	
+	
+    $scope.viewPrescription = function (data) {
+    	
+    	
+    	$scope.bringPresCribedDrugs(data.appointmentID);
+    	$scope.bringPrescribedInv(data.appointmentID);
+    	$scope.bringPrescribedAdvice(data.appointmentID);
+    	$scope.bringPrescribedVital(data.appointmentID);
+    	$scope.bringPrescribedComplain(data.appointmentID);
+    	$scope.bringPrescribedOBS(data.appointmentID, data.patientID);
+    	$scope.bringPrescribedHistory(data.appointmentID, data.patientID);
+    	
+    	$scope.showPrescriptionView = true;
+    	$scope.prescriptionViewDate = data.date;
+    };
+    
+    $scope.addToPrescription = function (state, requestedData, queryNo){
+    	
+    	requestedData.addedToPrescription = state;
+    	if(state){
+    		
+    		var dataString = "query="+ queryNo + '&requestedID=' + requestedData.id;
+
+            $http({
+                method: 'POST',
+                url: "phpServices/oldPrescription/oldPrescription.php",
+                data: dataString,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function (result) {
+            	
+            });
+    	}else{
+    		alert("Please remove it from Prescription Page");
+    		
+    		requestedData.addedToPrescription = !state;
+    	}
+    };
+
+	$scope.inIt = function (){
+		$scope.bringMenu();
+		$scope.bringPatientInfo();
+		
+	};
+	
+	(function(){
+		$scope.inIt();
+    })()
+
+	
+});
+
+
+app.controller('OldPrescriptionController.ViewPrescriptionController', function($scope, $modalInstance, data, $http) {
+	
+	
+	$(".modal-dialog").addClass('finalStepWidth');
+	angular.element(".modal-dialog").addClass('finalStepWidth');
+	$scope.$apply();
+	
+	
+	
+	$scope.cancelNewPatient = function (){
+		$modalInstance.dismiss('cancel');
+	};
+	
+	
+});
---
+++ phpServices/appointment/appointmentHelper.php
@@ -0,0 +1,126 @@
+<?php
+
+session_start();
+include('../config.inc');
+include('../commonServices/appointmentService.php');
+
+if (!isset($_SESSION['username'])) {
+	header('Location: index.php');
+}
+
+$username=$_SESSION['username'];
+$date=date("Y-m-d");
+$query_no=  $_POST['query'];
+$time_now=mktime(date('h')+6,date('i'),date('s'));
+$time=date('h:i:s',$time_now);
+$date=date("y-m-d");
+
+if($query_no== 0){
+	
+	$sql=mysql_query("SELECT 
+						d.doctorID, d.doctorCode, d.password, d.name, d.sex, d.age, d.phone, ds.category, ds.state, ds.prescriptionStyle, 
+						ds.patientType, ds.patientState, ds.hospitalID, ds.photoSupport, ds.personCodeInitial, dc.name AS categoreyName, ds.pdfPage
+					FROM doctor d
+					JOIN doctorsettings ds ON d.doctorID = ds.doctorID
+					JOIN doctorcategory dc ON ds.category = dc.id
+					WHERE d.doctorCode ='$username'");
+	$rec=mysql_fetch_array($sql);
+	echo json_encode($rec);
+}
+
+if($query_no==1){
+	
+	$filteredDate = $_POST['filteredDate'];
+	
+	echo getAppointment($username, $filteredDate);
+}
+
+if($query_no==2){
+	
+	$patientCode = mysql_real_escape_string($_POST['dotorPatInitial']);
+	$doctorCode=$_POST['doctorCode'];
+	$doctorID=$_POST['doctorID'];
+	$name = $_POST['name'];
+	$address = $_POST['address'];
+	$age = $_POST['age'];
+	$sex= $_POST['sex'];
+	$phone = $_POST['phone'];
+	
+	$appointmentType =  0;
+	
+	$sql="INSERT INTO `patient`( `patientCode`, `name`, `age`, `sex`, `address`, `phone`) VALUES ( '$patientCode', '$name', '$age' , '$sex', '$address', '$phone')";
+	
+	mysql_query($sql);
+	
+	$data = addAppointMent($doctorCode, $patientCode, $appointmentType,$doctorID, $date, $time, $username);
+	
+	mysql_query("UPDATE `doctorsettings` SET `personCodeInitial`=  personCodeInitial + 1 WHERE doctorID = $doctorID");
+	
+	echo $data;
+}
+else if($query_no==3){
+	
+	
+	$date = $_POST['filteredDate'];
+	$doctorCode= $_POST['doctorCode'];
+	$patientCode = $_POST['patientCode'];
+	$doctorID=$_POST['doctorID'];
+	
+	$appointmentType =  1;
+	
+	$data = addAppointMent($doctorCode, $patientCode, $appointmentType,$doctorID, $date, $time, $username);
+	
+	
+	
+	echo $data;
+
+	
+}
+else if($query_no==4){
+            $_SESSION['appointmentID']=$_POST['appointmentID'];
+            $_SESSION['patientCode']=$_POST['patientCode'];
+            $_SESSION['patientID']=$_POST['patientID'];
+	
+}else if($query_no==5){
+	$queryString=$_POST['data'];
+	$sql ="SELECT `patientID`, `patientCode`, `name`, `age`, `sex`, `address`, `phone` FROM `patient` WHERE name LIKE '%" . $queryString . "%' LIMIT 10";
+	$result=mysql_query($sql);
+	//echo $sql;
+	 $data = array();
+	while ($row=mysql_fetch_array($result)){
+		array_push($data,$row);
+	}
+	echo json_encode($data); 
+}else if($query_no==6){
+	session_destroy();
+	
+}else if($query_no==7){
+	$queryString=$_POST['data'];
+	$sql ="SELECT `patientID`, `patientCode`, `name`, `age`, `sex`, `address`, `phone` FROM `patient` WHERE patientCode LIKE '%" . $queryString . "%' LIMIT 10";
+	$result=mysql_query($sql);
+	//echo $sql;
+	 $data = array();
+	while ($row=mysql_fetch_array($result)){
+		array_push($data,$row);
+	}
+	echo json_encode($data); 
+	
+	
+}else if($query_no==8){
+	$queryString=$_POST['data'];
+	$sql ="SELECT `patientID`, `patientCode`, `name`, `age`, `sex`, `address`, `phone` FROM `patient` WHERE phone LIKE '%" . $queryString . "%' LIMIT 10";
+	$result=mysql_query($sql);
+	//echo $sql;
+	 $data = array();
+	while ($row=mysql_fetch_array($result)){
+		array_push($data,$row);
+	}
+	echo json_encode($data); 
+}else if($query_no==9){
+	$appointmentID=$_POST['appointmentID'];
+	mysql_query("DELETE FROM `appointment` WHERE `appointmentID` = $appointmentID");
+}else if($query_no==10){
+	$appointmentID=$_POST['appointmentID'];
+	mysql_query("UPDATE `appointment` SET`status`= 2 WHERE `appointmentID` = $appointmentID");
+}
+?>
